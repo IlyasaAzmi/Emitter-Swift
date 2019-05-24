1c5aab7c060af1448f69c0045209dc6ccee70deb
Ilyasa Azmi <theazmi99ilyasa@gmail.com>
2019-05-24 22:23:22 +0700

    Initial Commit

---
+++ Emitter-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emitter-Swift
+//
+//  Created by Ilyasa Azmi on 24/05/19.
+//  Copyright © 2019 co.ilyasa.azmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
